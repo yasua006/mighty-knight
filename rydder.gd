7d328fee43cce5a0abc6ba10b5e13e7b7aef2915
Ivan <iadomocmat@gmail.com>
2025-11-17 09:06:17 +0100

    Endret hastighet

--- rydder.gd
+++ rydder.gd
@@ -8,16 +8,16 @@ func _process(_delta: float) -> void:
 	
 	# bevegelser
 	if Input.is_action_pressed("move_right"):
-		velocity.x += 20
+		velocity.x += 5
 	if Input.is_action_pressed("move_left"):
-		velocity.x -= 20
+		velocity.x -= 5
 	if Input.is_action_pressed("move_down"):
-		velocity.y += 20
+		velocity.y += 5
 	if Input.is_action_pressed("move_up"):
-		velocity.y -= 20
+		velocity.y -= 5
 	
 	# håndtere samtidig bevegelser
-	if velocity.length() < 0:
+	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 	
 	# bevege
