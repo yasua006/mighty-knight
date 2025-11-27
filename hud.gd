4ab1df33ffa255d0e3f0ec527cd4d58249e99acb
yasua006 <156091445+yasua006@users.noreply.github.com>
2025-11-27 09:16:35 +0100

    hud og spill meny feil rett (rydder til ridder)

--- hud.gd
+++ hud.gd
@@ -1,7 +1,7 @@
 extends Control
 
-var rydder = null
-var ny_rydder = null
+var ridder = null
+var ny_ridder = null
 
 var old_speed: int = 0
 
@@ -10,36 +10,31 @@ func _ready() -> void:
 	$level_label.hide()
 	$level_label_timer.start()
 	
-	# få hastigheten før 0 stilling
-	rydder = get_tree().root.get_child(1).get_child(1)
-	old_speed = rydder.speed
+	ridder = get_tree().root.get_child(1).get_child(1)
+	old_speed = ridder.speed
 	
-	# 0 hastighet når på menyen
-	rydder.speed = 0
+	ridder.speed = 0
 
 
 func _on_level_label_timer_timeout() -> void:
 	$level_label.show()
 	
-	# håndtere animasjoner av
 	if not $level_label/anim.active:
 		$level_label.position = Vector2(300, 150)
 		$anim_av_timer.start()
 	else:
-		# vis nivå animasjon
 		$level_label/anim.play("fade_label")
 
 
-func hjelper() -> void:
+func anim_hjelper() -> void:
 	$level_label.hide()
 	
-	# få førrige hastigheten
-	rydder.speed = old_speed
+	ridder.speed = old_speed
 
 
 func _on_anim_av_timer_timeout() -> void:
-	hjelper()
+	anim_hjelper()
 
 
 func _on_anim_animation_finished(_anim_name: StringName) -> void:
-	hjelper()
+	anim_hjelper()
